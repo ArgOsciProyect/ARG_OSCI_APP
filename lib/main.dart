e5dacc629365fa55b08e14146dc4854ccd10928e
Jolx77 <joaquin.otalora@mi.unc.edu.ar>
2024-11-27 10:39:17 -0300

    No mas services en main
    Signed-off-by: Jolx77 <joaquin.otalora@mi.unc.edu.ar>

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'config/app_theme.dart';
 import 'features/setup/screens/setup_screen.dart';
-import 'features/socket/domain/services/socket_service.dart';
-import 'features/setup/domain/services/setup_service.dart';
-import 'features/setup/providers/setup_provider.dart';
-import 'features/http/domain/models/http_config.dart'; // Importar HttpConfig
+import 'config/initializer.dart'; // Import the Initializer
 import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await requestPermissions();
 
-  // Initialize the global services
-  final globalSocketService = SocketService();
-  final setupService = SetupService(globalSocketService, HttpConfig('http://192.168.4.1:81'));
-  Get.put(globalSocketService);
-  Get.put(setupService);
-
-  // Initialize the providers
-  Get.put(SetupProvider(setupService));
+  // Initialize dependencies
+  await Initializer.init();
 
   runApp(MyApp());
 }
