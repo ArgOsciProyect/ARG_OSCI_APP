ecdef82a7655031ce479f341ee55e79703959963
Jolx77 <joaquin.otalora@mi.unc.edu.ar>
2025-01-13 12:38:51 -0300

    GA fix

--- reorganice_proyect.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p lib/features/graph/domain/models
-mkdir -p lib/features/graph/domain/repository
-mkdir -p lib/features/graph/domain/services
-mkdir -p lib/features/graph/providers
-mkdir -p lib/features/graph/screens
-mkdir -p lib/features/graph/widgets
