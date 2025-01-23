3ad6c57fac9c1f4fe07c2f230c613efa4ac2e3bb
Jolx77 <joaquin.otalora@mi.unc.edu.ar>
2025-01-23 09:49:02 -0300

    Desplazamientos

--- lib/features/graph/providers/line_chart_provider.dart
+++ lib/features/graph/providers/line_chart_provider.dart
@@ -1,4 +1,6 @@
 // lib/features/graph/providers/line_chart_provider.dart
+import 'dart:ui';
+
 import 'package:get/get.dart';
 import '../domain/services/line_chart_service.dart';
 import '../domain/models/data_point.dart';
@@ -12,13 +14,18 @@ class LineChartProvider extends GetxController {
   final _timeScale = RxDouble(1.0);
   final _valueScale = RxDouble(1.0);
   final _isPaused = false.obs;
+  final _horizontalOffset = RxDouble(0.0);
+  final _verticalOffset = RxDouble(0.0);
+  Timer? _incrementTimer;
 
   // Getters
   List<DataPoint> get dataPoints => _dataPoints.value;
   double get timeScale => _timeScale.value;
   double get valueScale => _valueScale.value;
   bool get isPaused => _isPaused.value;
-  
+  double get horizontalOffset => _horizontalOffset.value;
+  double get verticalOffset => _verticalOffset.value;
+
   LineChartProvider(this._lineChartService) {
     _dataSubscription = _lineChartService.dataStream.listen((points) {
       _dataPoints.value = points;
@@ -42,20 +49,79 @@ class LineChartProvider extends GetxController {
     _valueScale.value = 1.0;
   }
 
+  void resetOffsets() {
+    _horizontalOffset.value = 0.0;
+    _verticalOffset.value = 0.0;
+  }
+
+  void setHorizontalOffset(double offset) {
+    _horizontalOffset.value = offset;
+  }
+
+  void setVerticalOffset(double offset) {
+    _verticalOffset.value = offset;
+  }
+
+  void incrementTimeScale() {
+    setTimeScale(timeScale * 1.02);
+  }
+
+  void decrementTimeScale() {
+    setTimeScale(timeScale * 0.98);
+  }
+
+  void incrementValueScale() {
+    setValueScale(valueScale * 1.02);
+  }
+
+  void decrementValueScale() {
+    setValueScale(valueScale * 0.98);
+  }
+
+  void incrementHorizontalOffset() {
+    setHorizontalOffset(horizontalOffset + 0.01); // Reducir de 0.1 a 0.001
+  }
+
+  void decrementHorizontalOffset() {
+    setHorizontalOffset(horizontalOffset - 0.01); // Reducir de 0.1 a 0.001
+  }
+
+  void incrementVerticalOffset() {
+    setVerticalOffset(verticalOffset + 0.1);
+  }
+
+  void decrementVerticalOffset() {
+    setVerticalOffset(verticalOffset - 0.1);
+  }
+
+  void startIncrementing(VoidCallback callback) {
+    callback();
+    _incrementTimer?.cancel();
+    _incrementTimer = Timer.periodic(const Duration(milliseconds: 100), (_) {
+      callback();
+    });
+  }
+
+  void stopIncrementing() {
+    _incrementTimer?.cancel();
+    _incrementTimer = null;
+  }
+
+  @override
+  void onClose() {
+    _incrementTimer?.cancel();
+    _dataSubscription?.cancel();
+    _lineChartService.dispose();
+    super.onClose();
+  }
+
   void pause() {
     _isPaused.value = true;
     _lineChartService.pause();
   }
 
   void resume() {
-  _isPaused.value = false;
-  _lineChartService.resume();
-}
-
-  @override
-  void onClose() {
-    _dataSubscription?.cancel();
-    _lineChartService.dispose();
-    super.onClose();
+    _isPaused.value = false;
+    _lineChartService.resume();
   }
 }
--- lib/features/graph/widgets/line_chart.dart
+++ lib/features/graph/widgets/line_chart.dart
@@ -4,16 +4,19 @@ import 'package:get/get.dart';
 import '../../graph/domain/models/data_point.dart';
 import '../../graph/providers/line_chart_provider.dart';
 import '../providers/data_provider.dart';
+import 'dart:async';
 
-// ignore: unused_element
-late Size _size;
-// Mover el gráfico hacia arriba y a la derecha
 const double _offsetY = 15;
 const double _offsetX = 50;
 const double _sqrOffsetBot = 15;
 
 class LineChart extends StatelessWidget {
   const LineChart({super.key});
+
+  void _startIncrementing(Function() callback) {
+    callback();
+    Timer.periodic(const Duration(milliseconds: 100), (_) => callback());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,6 +46,8 @@ class LineChart extends StatelessWidget {
                               graphProvider.getDistance(),
                               graphProvider.getScale(),
                               Theme.of(context).scaffoldBackgroundColor,
+                              lineChartProvider.horizontalOffset,
+                              lineChartProvider.verticalOffset,
                             ),
                           );
                   }),
@@ -56,48 +61,128 @@ class LineChart extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              IconButton(
-                icon: Obx(() => Icon(
-                      lineChartProvider.isPaused
-                          ? Icons.play_arrow
-                          : Icons.pause,
-                    )),
-                color: Colors.black,
-                onPressed: () => lineChartProvider.isPaused
-                    ? lineChartProvider.resume()
-                    : lineChartProvider.pause(),
+              // Controles principales
+              Row(
+                children: [
+                  IconButton(
+                    icon: Obx(() => Icon(
+                          lineChartProvider.isPaused
+                              ? Icons.play_arrow
+                              : Icons.pause,
+                        )),
+                    color: Colors.black,
+                    onPressed: () => lineChartProvider.isPaused
+                        ? lineChartProvider.resume()
+                        : lineChartProvider.pause(),
+                  ),
+                  GestureDetector(
+                    onTapDown: (_) => lineChartProvider.decrementTimeScale(),
+                    onLongPress: () => lineChartProvider.startIncrementing(
+                        lineChartProvider.decrementTimeScale),
+                    onLongPressUp: lineChartProvider.stopIncrementing,
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_left),
+                      color: Colors.black,
+                      onPressed: null,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTapDown: (_) => lineChartProvider.incrementTimeScale(),
+                    onLongPress: () => lineChartProvider.startIncrementing(
+                        lineChartProvider.incrementTimeScale),
+                    onLongPressUp: lineChartProvider.stopIncrementing,
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_right),
+                      color: Colors.black,
+                      onPressed: null,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTapDown: (_) => lineChartProvider.incrementValueScale(),
+                    onLongPress: () => lineChartProvider.startIncrementing(
+                        lineChartProvider.incrementValueScale),
+                    onLongPressUp: lineChartProvider.stopIncrementing,
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_upward),
+                      color: Colors.black,
+                      onPressed: null,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTapDown: (_) => lineChartProvider.decrementValueScale(),
+                    onLongPress: () => lineChartProvider.startIncrementing(
+                        lineChartProvider.decrementValueScale),
+                    onLongPressUp: lineChartProvider.stopIncrementing,
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_downward),
+                      color: Colors.black,
+                      onPressed: null,
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.autorenew),
+                    color: Colors.black,
+                    onPressed: () {
+                      final size = MediaQuery.of(context).size;
+                      graphProvider.autoset(size.height, size.width);
+                      lineChartProvider.resetOffsets();
+                    },
+                  ),
+                ],
               ),
-              IconButton(
-                icon: const Icon(Icons.arrow_left),
-                color: Colors.black,
-                onPressed: () => lineChartProvider
-                    .setTimeScale(lineChartProvider.timeScale * 0.9),
-              ),
-              IconButton(
-                icon: const Icon(Icons.arrow_right),
-                color: Colors.black,
-                onPressed: () => lineChartProvider
-                    .setTimeScale(lineChartProvider.timeScale * 1.1),
-              ),
-              IconButton(
-                icon: const Icon(Icons.arrow_upward),
-                color: Colors.black,
-                onPressed: () => lineChartProvider
-                    .setValueScale(lineChartProvider.valueScale * 1.1),
-              ),
-              IconButton(
-                icon: const Icon(Icons.arrow_downward),
-                color: Colors.black,
-                onPressed: () => lineChartProvider
-                    .setValueScale(lineChartProvider.valueScale * 0.9),
-              ),
-              IconButton(
-                icon: const Icon(Icons.autorenew),
-                color: Colors.black,
-                onPressed: () {
-                  final size = MediaQuery.of(context).size;
-                  graphProvider.autoset(size.height, size.width);
-                },
+              const SizedBox(width: 20),
+              // Controles de desplazamiento
+              Row(
+                children: [
+                  GestureDetector(
+                    onTapDown: (_) =>
+                        lineChartProvider.decrementHorizontalOffset(),
+                    onLongPress: () => lineChartProvider.startIncrementing(
+                        lineChartProvider.decrementHorizontalOffset),
+                    onLongPressUp: lineChartProvider.stopIncrementing,
+                    child: IconButton(
+                      icon: const Icon(Icons.keyboard_arrow_left),
+                      color: Colors.black,
+                      onPressed: null,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTapDown: (_) =>
+                        lineChartProvider.incrementHorizontalOffset(),
+                    onLongPress: () => lineChartProvider.startIncrementing(
+                        lineChartProvider.incrementHorizontalOffset),
+                    onLongPressUp: lineChartProvider.stopIncrementing,
+                    child: IconButton(
+                      icon: const Icon(Icons.keyboard_arrow_right),
+                      color: Colors.black,
+                      onPressed: null,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTapDown: (_) =>
+                        lineChartProvider.incrementVerticalOffset(),
+                    onLongPress: () => lineChartProvider.startIncrementing(
+                        lineChartProvider.incrementVerticalOffset),
+                    onLongPressUp: lineChartProvider.stopIncrementing,
+                    child: IconButton(
+                      icon: const Icon(Icons.keyboard_arrow_up),
+                      color: Colors.black,
+                      onPressed: null,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTapDown: (_) =>
+                        lineChartProvider.decrementVerticalOffset(),
+                    onLongPress: () => lineChartProvider.startIncrementing(
+                        lineChartProvider.decrementVerticalOffset),
+                    onLongPressUp: lineChartProvider.stopIncrementing,
+                    child: IconButton(
+                      icon: const Icon(Icons.keyboard_arrow_down),
+                      color: Colors.black,
+                      onPressed: null,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
@@ -115,13 +200,23 @@ class LineChartPainter extends CustomPainter {
   final double distance;
   final double voltageScale;
   final Color backgroundColor;
+  final double horizontalOffset;
+  final double verticalOffset;
 
-  LineChartPainter(this.dataPoints, this.timeScale, this.valueScale, this.maxX,
-      this.distance, this.voltageScale, this.backgroundColor);
+  LineChartPainter(
+    this.dataPoints,
+    this.timeScale,
+    this.valueScale,
+    this.maxX,
+    this.distance,
+    this.voltageScale,
+    this.backgroundColor,
+    this.horizontalOffset,
+    this.verticalOffset,
+  );
 
   @override
   void paint(Canvas canvas, Size size) {
-    _size = size;
     final paint = Paint()
       ..color = Colors.blue
       ..strokeWidth = 2;
@@ -129,6 +224,11 @@ class LineChartPainter extends CustomPainter {
     final gridPaint = Paint()
       ..color = Colors.grey
       ..strokeWidth = 0.5;
+
+    final zeroPaint = Paint()
+      ..color = Colors.red
+      ..strokeWidth = 1
+      ..style = PaintingStyle.stroke;
 
     final borderPaint = Paint()
       ..color = Colors.black
@@ -143,22 +243,27 @@ class LineChartPainter extends CustomPainter {
       ..color = Colors.white
       ..style = PaintingStyle.fill;
 
-    const double offsetY = _offsetY;
-    const double offsetX = _offsetX;
-    const double sqrOffsetBot = _sqrOffsetBot;
-
     // Drawing area dimensions
-    final drawingWidth = size.width - offsetX;
-    final drawingHeight = size.height - offsetY - sqrOffsetBot;
-    final centerY = offsetY + drawingHeight / 2;
+    final drawingWidth = size.width - _offsetX;
+    final drawingHeight = size.height - _offsetY - _sqrOffsetBot;
+    final centerY = _offsetY + drawingHeight / 2;
 
     // Background y áreas
-    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, offsetY), backgroundPaint);
-    canvas.drawRect(Rect.fromLTWH(0, offsetY, offsetX, size.height - offsetY),
+    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, _offsetY), backgroundPaint);
+    canvas.drawRect(
+        Rect.fromLTWH(0, _offsetY, _offsetX, size.height - _offsetY),
         backgroundPaint);
     canvas.drawRect(
-        Rect.fromLTWH(offsetX, offsetY, drawingWidth, drawingHeight),
+        Rect.fromLTWH(_offsetX, _offsetY, drawingWidth, drawingHeight),
         chartBackgroundPaint);
+
+    // Dibujar línea de cero
+    final zeroY = centerY - (verticalOffset * drawingHeight / 2);
+    canvas.drawLine(
+      Offset(_offsetX, zeroY),
+      Offset(size.width, zeroY),
+      zeroPaint,
+    );
 
     final textPainter = TextPainter(
       textAlign: TextAlign.center,
@@ -167,30 +272,32 @@ class LineChartPainter extends CustomPainter {
 
     // Eje X (tiempo)
     for (int i = 0; i <= 10; i++) {
-      final x = offsetX + (drawingWidth * i / 10);
-      canvas.drawLine(
-          Offset(x, offsetY), Offset(x, size.height - sqrOffsetBot), gridPaint);
+      final x = _offsetX + (drawingWidth * i / 10);
+      canvas.drawLine(Offset(x, _offsetY),
+          Offset(x, size.height - _sqrOffsetBot), gridPaint);
 
-      final timeValue = (x - offsetX) / timeScale * 1e6;
+      final timeValue = ((x - _offsetX) / timeScale -
+              horizontalOffset * drawingWidth / timeScale) *
+          1e6;
       textPainter.text = TextSpan(
         text: '${timeValue.toStringAsFixed(1)} µs',
-        style: TextStyle(color: Colors.black, fontSize: 10),
+        style: const TextStyle(color: Colors.black, fontSize: 10),
       );
       textPainter.layout();
       textPainter.paint(canvas,
-          Offset(x - textPainter.width / 2, size.height - sqrOffsetBot + 5));
+          Offset(x - textPainter.width / 2, size.height - _sqrOffsetBot + 5));
     }
 
     // Para el eje Y - modificar para que se ajuste con la escala
     for (int i = -5; i <= 5; i++) {
       final y = centerY - (i * drawingHeight / 10);
-      canvas.drawLine(Offset(offsetX, y), Offset(size.width, y), gridPaint);
+      canvas.drawLine(Offset(_offsetX, y), Offset(size.width, y), gridPaint);
 
-      // El valor debe ser proporcional a la posición en la pantalla
-      final value = (centerY - y) / (drawingHeight / 2) / valueScale;
+      final value =
+          ((centerY - y) / (drawingHeight / 2) - verticalOffset) / valueScale;
       textPainter.text = TextSpan(
         text: '${value.toStringAsFixed(2)} V',
-        style: TextStyle(color: Colors.black, fontSize: 10),
+        style: const TextStyle(color: Colors.black, fontSize: 10),
       );
       textPainter.layout();
       textPainter.paint(canvas, Offset(5, y - textPainter.height / 2));
@@ -199,50 +306,59 @@ class LineChartPainter extends CustomPainter {
     // Dibujar puntos de datos
     if (dataPoints.length > 1) {
       for (int i = 0; i < dataPoints.length - 1; i++) {
-        var p1 = Offset(dataPoints[i].x * timeScale + offsetX,
-            centerY - (dataPoints[i].y * valueScale * drawingHeight / 2));
-        var p2 = Offset(dataPoints[i + 1].x * timeScale + offsetX,
-            centerY - (dataPoints[i + 1].y * valueScale * drawingHeight / 2));
+        var p1 = Offset(
+            (dataPoints[i].x * timeScale + horizontalOffset * drawingWidth) +
+                _offsetX,
+            centerY -
+                ((dataPoints[i].y + verticalOffset) *
+                    valueScale *
+                    drawingHeight /
+                    2));
+
+        var p2 = Offset(
+            (dataPoints[i + 1].x * timeScale +
+                    horizontalOffset * drawingWidth) +
+                _offsetX,
+            centerY -
+                ((dataPoints[i + 1].y + verticalOffset) *
+                    valueScale *
+                    drawingHeight /
+                    2));
 
         // Skip if both points are outside left boundary
-        if (p1.dx < offsetX && p2.dx < offsetX) {
-          continue;
-        }
+        if (p1.dx < _offsetX && p2.dx < _offsetX) continue;
 
         // Skip if both points are outside right boundary
-        if (p1.dx > size.width && p2.dx > size.width) {
-          continue;
-        }
+        if (p1.dx > size.width && p2.dx > size.width) continue;
 
         // Clip vertical boundaries
-        if (p1.dy < offsetY) {
-          p1 = Offset(p1.dx, offsetY);
-        } else if (p1.dy > size.height - sqrOffsetBot) {
-          p1 = Offset(p1.dx, size.height - sqrOffsetBot);
+        if (p1.dy < _offsetY) {
+          p1 = Offset(p1.dx, _offsetY);
+        } else if (p1.dy > size.height - _sqrOffsetBot) {
+          p1 = Offset(p1.dx, size.height - _sqrOffsetBot);
         }
 
-        if (p2.dy < offsetY) {
-          p2 = Offset(p2.dx, offsetY);
-        } else if (p2.dy > size.height - sqrOffsetBot) {
-          p2 = Offset(p2.dx, size.height - sqrOffsetBot);
+        if (p2.dy < _offsetY) {
+          p2 = Offset(p2.dx, _offsetY);
+        } else if (p2.dy > size.height - _sqrOffsetBot) {
+          p2 = Offset(p2.dx, size.height - _sqrOffsetBot);
         }
 
         // Clip horizontal boundaries
-        if (p1.dx < offsetX) {
-          // Calculate intersection with left boundary
+        if (p1.dx < _offsetX) {
           final slope = (p2.dy - p1.dy) / (p2.dx - p1.dx);
-          final y = p1.dy + (offsetX - p1.dx) * slope;
-          p1 = Offset(offsetX, y);
+          final y = p1.dy + (_offsetX - p1.dx) * slope;
+          p1 = Offset(_offsetX, y);
         } else if (p1.dx > size.width) {
           final slope = (p2.dy - p1.dy) / (p2.dx - p1.dx);
           final y = p1.dy + (size.width - p1.dx) * slope;
           p1 = Offset(size.width, y);
         }
 
-        if (p2.dx < offsetX) {
+        if (p2.dx < _offsetX) {
           final slope = (p2.dy - p1.dy) / (p2.dx - p1.dx);
-          final y = p2.dy + (offsetX - p2.dx) * slope;
-          p2 = Offset(offsetX, y);
+          final y = p2.dy + (_offsetX - p2.dx) * slope;
+          p2 = Offset(_offsetX, y);
         } else if (p2.dx > size.width) {
           final slope = (p2.dy - p1.dy) / (p2.dx - p1.dx);
           final y = p2.dy + (size.width - p2.dx) * slope;
@@ -255,7 +371,7 @@ class LineChartPainter extends CustomPainter {
 
     // Borde
     canvas.drawRect(
-        Rect.fromLTWH(offsetX, offsetY, drawingWidth, drawingHeight),
+        Rect.fromLTWH(_offsetX, _offsetY, drawingWidth, drawingHeight),
         borderPaint);
   }
 
